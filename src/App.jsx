3075e561943f81e5a6f2b150be7d9f8d54e8fcd8
shubh-angi01 <shubhangipriya2004@gmail.com>
2025-04-04 19:43:23 +0530

    created react app and installed tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+ function App() {
+  return (
+    <h1 className=" flex justify-center items-center h-screen font-bold font-xxl">
+      Zidio E-commerce App!!
+    </h1>
+  )
+}
+export default App;

ae80a732c421931afe73f56e1657164e77f5e37b
Ayush Verma <iayush001@gmail.com>
2025-04-07 19:47:48 +0530

    Updated the Hero Section Image for frontend in src/assets/images

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className=  " text-center text-sm p-6 text-gray-500 border-t mt-10">
+    <footer className=" text-center text-sm p-6 text-gray-500 border-t mt-10">
       &copy; 2025 Zidio's E-Commerce | All rights reserved.
     </footer>
   );
--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import heroImg from "../assets/hero.jpg";
 const HeroSection = () => {
   return (
     <section className="w-full h-[80vh] bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: `url(${heroImg})` }}>
-      <h1 className="text-white text-4xl md:text-6xl font-bold backdrop-blur bg-black/30 px-6 py-4 rounded"> STYLISH COLLECTIONS</h1>
+      <h1 className="text-white text-4xl md:text-6xl font-bold backdrop-blur bg-black/30 px-6 py-4 rounded">ALL NEW COLLECTION</h1>
     </section>
   );
 };
